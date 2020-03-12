4a6a7f16301927ed0a6c5c45cd78be1085e5df8a
Michael-Kochis <rhishisikk@gmail.com>
2020-03-12 15:52:29 -0400

    ERSUserRole and associated tests.

--- src/main/java/com/revature/enums/ERSUserRole.java
+++ src/main/java/com/revature/enums/ERSUserRole.java
@@ -2,4 +2,20 @@ package com.revature.enums;
 
 public enum ERSUserRole {
 	ERS_UR_EMP, ERS_UR_FIN_ADM;
+	
+	public static long typeToLong(ERSUserRole ersu) {
+		if (ersu.equals(ERS_UR_EMP)) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+	
+	public static ERSUserRole longToType(long ersu) {
+		int x = ((int) ersu);
+		switch(x) {
+		  case 1: return ERSUserRole.ERS_UR_FIN_ADM;
+		  default: return ERSUserRole.ERS_UR_EMP;
+		}
+	}
 }
--- src/test/java/com/revature/test/enums/AllEnumTests.java
+++ src/test/java/com/revature/test/enums/AllEnumTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({ERSReimbursementStatusTest.class, ERSReimbursementTypeTest.class, ERSUserRoleTest.class })
 public class AllEnumTests {
 
 }
